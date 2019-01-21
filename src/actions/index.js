e254e5cdb10aab07235e5cf255e3c0fd1aa20076
Ryan <falcsmike7@gmail.com>
2019-01-21 01:00:20 -0500

    async action creator defined

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-  const repsonse = await jsonPlaceholder.get('/posts')
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts')
 
-  return {
-    type: 'FETCH_POSTS',
-    paylod: response
-  }
+    dispatch({ type: 'FETCH_POSTS', payload: response })
 }
